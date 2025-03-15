b4563b473ddb647a46c9da0a68bf15a4fbc63dfa
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-03-15 23:42:47 +0200

    Add dimond square and worley noise algorithms, add map helpers, README edited

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🏔Terrain generation
+
+This is course project focuses on **procedural terrain generation** for video games. It provides an implementation of various algorithms to create diverse landscapes, allowing users to explore different methods of terrain synthesis. The project was developed on the Godot game engine using the C# programming language.
+
+## Implemented Algorithms
+
+### Perlin Noise Terrain
+- **Youtube playlist:** [https://youtube.com/playlist?list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3&si=jVkk8ELhfEQ31YH0](https://youtube.com/playlist?list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3&si=jVkk8ELhfEQ31YH0)
+- **GitHub 1:** [https://github.com/formalatist/Perlin/tree/master](https://github.com/formalatist/Perlin/tree/master)
---
+++ Utilities/MapHelpers.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace TerrainGenerationApp.Utilities;
+
+public static class MapHelpers
+{
+    private static readonly (int, int)[] DirectionsHvd =
+    {
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+        (1, 1)
+    };
+
+
+    public static float[,] SmoothMap(float[,] map)
+    {
+        var h = map.GetLength(0);
+        var w = map.GetLength(1);
+        var newMap = new float[h, w];
+
+        for (int y = 0; y < h; y++)
+        {
+            for (int x = 0; x < w; x++)
+            {
+                var sum = 0f;
+                var count = 0;
+
+                foreach (var (dy, dx) in DirectionsHvd)
+                {
+                    var nx = x + dx;
+                    var ny = y + dy;
+
+                    if (nx >= 0 && nx < w && ny >= 0 && ny < h)
+                    {
+                        sum += map[ny, nx];
+                        count++;
+                    }
+                }
+
+                newMap[y, x] = count > 0 ? sum / count : map[y, x];
+            }
+        }
+        return newMap;
+    }
+
+    public static float[,] GetSlopes(float[,] map)
+    {
+        int h = map.GetLength(0);
+        int w = map.GetLength(1);
+        float[,] slopes = new float[h, w];
+
+        float HeightOrDefault(int x, int y) =>
+            x >= 0 && x < w && y >= 0 && y < h ? map[y, x] : 0f;
+
+        for (int y = 0; y < h; y++)
+        {
+            for (int x = 0; x < w; x++)
+            {
+                float vecX = 0.5f * (HeightOrDefault(x + 1, y) - HeightOrDefault(x - 1, y));
+                float vecY = 0.5f * (HeightOrDefault(x, y + 1) - HeightOrDefault(x, y - 1));
+                slopes[y, x] = MathF.Sqrt(vecX * vecX + vecY * vecY);
+            }
+        }
+        return slopes;
+    }
+
+    public static void AddHeight(float[,] map, float value, float lowerBound = 0.0f, float upperBound = 1.0f)
+    {
+        int h = map.GetLength(0);
+        int w = map.GetLength(1);
+
+        for (int y = 0; y < h; y++)
+        {
+            for (int x = 0; x < w; x++)
+            {
+                map[y, x] = Math.Clamp(map[y, x] + value, lowerBound, upperBound);
+            }
+        }
+    }
+
+    public static void MultiplyHeight(float[,] map, float value, float lowerBound = 0.0f, float upperBound = 1.0f)
+    {
+        int h = map.GetLength(0);
+        int w = map.GetLength(1);
+
+        for (int y = 0; y < h; y++)
+        {
+            for (int x = 0; x < w; x++)
+            {
+                map[y, x] = Math.Clamp(map[y, x] * value, lowerBound, upperBound);
+            }
+        }
+    }
+}
+
